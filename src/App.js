895bb1619f4795b7c657c604d3be5628088dc9e3
Octavio Pezoa <octavio.pezoa@gmail.com>
2019-06-25 18:58:51 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// importo data de prueba
+import task from './example/task.json';
+console.log(task);
+
+
+class App extends Component {
+
+  state= {
+    task : task
+  }
+
+  render() {
+    return <div>{ this.state.task }</div>
+  }
 }
 
 export default App;
