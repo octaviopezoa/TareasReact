c4a315d5e8b437bc41d35bcc466d44475a5e4fa8
Octavio Pezoa <octaviopezoa@MacBook-Pro-de-Octavio.local>
2019-07-02 23:56:03 -0400

    Se utiliza props, estados y componentes reutilizables

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 
 // importo data de prueba
 import task from './example/task.json';
 console.log(task);
 
+// function HelloWorld(props){
+//   console.log(props);
+  
+//   return (
+//     <div id="hello">
+//     <h2>{props.mytext}</h2>
+//     <h4>{props.subt}</h4>
+//     </div>
+//   )
+// } 
 
-class App extends Component {
+class HelloWorld extends React.Component {
 
-  state= {
-    task : task
+  state = {
+    show : true
   }
 
-  render() {
-    return <div>{ this.state.task }</div>
+  toggle = () => {
+    this.setState({show: !this.state.show})
   }
+
+  render () {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h2>{this.props.mytext}</h2>
+          <h4>{this.props.subt}</h4>
+          <button onClick={this.toggle}>Cambia estado</button>
+        </div>
+      )
+    }
+    else {
+      return <div> 
+        <h4>sin datos</h4>
+        <button onClick={this.toggle}>volver</button>
+      </div>
+    }
+  }
+}
+
+
+
+function App() {
+  return (
+    <div>
+      <HelloWorld mytext="holo!!" subt="quiero"/>
+      <HelloWorld mytext="chaucha" subt="nada"/>
+      <HelloWorld mytext="por la chita" subt="y todo"/>
+    </div>
+  )
 }
 
 export default App;
