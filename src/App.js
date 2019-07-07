f142fda963c8161f8d02205e0c32064e5ffa7214
Octavio Pezoa <octaviopezoa@MacBook-Pro-de-Octavio.local>
2019-07-06 22:50:57 -0400

    Comienzo del diseno de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,60 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-// importo data de prueba
+
 import task from './example/task.json';
 console.log(task);
 
-// function HelloWorld(props){
-//   console.log(props);
-  
-//   return (
-//     <div id="hello">
-//     <h2>{props.mytext}</h2>
-//     <h4>{props.subt}</h4>
-//     </div>
-//   )
-// } 
 
-class HelloWorld extends React.Component {
+class App extends Component {
 
   state = {
-    show : true
+    task: task
   }
 
-  toggle = () => {
-    this.setState({show: !this.state.show})
-  }
-
-  render () {
-    if (this.state.show) {
-      return (
-        <div id="hello">
-          <h2>{this.props.mytext}</h2>
-          <h4>{this.props.subt}</h4>
-          <button onClick={this.toggle}>Cambia estado</button>
-        </div>
-      )
-    }
-    else {
-      return <div> 
-        <h4>sin datos</h4>
-        <button onClick={this.toggle}>volver</button>
-      </div>
-    }
-  }
-}
-
-
-
-function App() {
-  return (
-    <div>
-      <HelloWorld mytext="holo!!" subt="quiero"/>
-      <HelloWorld mytext="chaucha" subt="nada"/>
-      <HelloWorld mytext="por la chita" subt="y todo"/>
+  render() {
+    return <div>
+      { this.state.task.map(e => <p key ={e.id}> - {e.title} - {e.description} - {e.done} - {e.id}</p>) }
     </div>
-  )
+  }
 }
+
+
 
 export default App;
